d62509ee210853e2745f5be8463c937772068568
Shameek Poddar <shameek.poddar@gmail.com>
2016-09-26 23:34:20 +0000

    IInitial commit on Vagrant

---
+++ app.py
@@ -0,0 +1,81 @@
+import os
+import requests
+import re
+import operator
+import nltk
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+q = Queue(connection=conn)
+
+from models import *
+
+def count_and_save_words(url):
+	errors = []
+	try:
+		r = requests.get(url)
+	except:
+		errors.append("Unable to get URL. Please make sure it's valid and try again.")
+		return {"error": errors}
+	if r:
+		#Process text
+		raw = BeautifulSoup(r.text, 'html.parser').get_text()
+		nltk.data.path.append('./nltk_data/')
+		tokens = nltk.word_tokenize(raw)
+		text = nltk.Text(tokens)
+		#remove punctuation and count words
+		nonPunct = re.compile('.*[A-Za-z].*')
+		raw_words = [w for w in text if nonPunct.match(w)]
+		raw_word_count = Counter(raw_words)
+		#stop words
+		no_stop_words = [w for w in raw_words if w.lower() not in stops]
+		no_stop_words_count = Counter(no_stop_words)
+		#save results to db
+		try:
+			result = Result(
+				url=url,
+				result_all=raw_word_count,
+				result_no_stop_words=no_stop_words_count
+			)
+			db.session.add(result)
+			db.session.commit()
+			return result.id
+		except:
+			errors.append("Unable to add item to database.")
+			return {"error": errors}
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	results = {}
+	if request.method == "POST":
+		url = request.form['url']
+		if 'http://' not in url:
+			url = 'http://' + url
+		job = q.enqueue_call(
+			func=count_and_save_words, args=(url,), result_ttl=5000
+		)
+		print(job.get_id())
+	return render_template('index.html', results=results)
+	
+@app.route('/results/<job_key>', methods=['GET'])
+def get_results(job_key):
+	job = Job.fetch(job_key, connection=conn)
+	if job.is_finished:
+		return str(job.result), 200
+	else:
+		return "Nay!", 202
+
+if __name__ == '__main__':
+	print(os.environ['APP_SETTINGS'])
+	app.run()
