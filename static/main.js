ab4833266931d8d5e842db2aee90b72b027b3b5b
vagrant <vagrant@vagrant.vm>
2016-09-27 23:45:12 +0000

    Poll and display results from backend with AngularJS

--- static/main.js
+++ static/main.js
@@ -2,8 +2,27 @@
 	'use strict';
 	
 	angular.module('WordcountApp', [])
-		.controller('WordcountController', ['$scope', '$log', '$http', 
-			function($scope, $log, $http){
+		.controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+			function($scope, $log, $http, $timeout){
+				function getWordCount(jobID){
+					var timeout = ""
+					var poller = function(){
+						$http.get('/results/' + jobID)
+							.success(function(data, status, headers, config){
+								if(status === 202){
+									$log.log(data, status)
+								} else if(status === 200){
+									$scope.wordcounts = data
+									$timeout.cancel(timeout)
+									return false
+								}
+								//call poller till timeout is cancelled
+								timeout = $timeout(poller, 2000)
+							})
+					}
+					poller()
+				}
+	
 				$scope.getResults = function(){
 					$log.log("test")
 					var userInput = $scope.url
@@ -11,13 +30,13 @@
 					$http.post('/start', {"url": userInput})
 						.success(function(results){
 							$log.log(results)
+							getWordCount(results)
 						}).error(function(error){
 							$log.log(error)
 						});
 				}
 			}
 		])
-	
 	
 	
 }());
