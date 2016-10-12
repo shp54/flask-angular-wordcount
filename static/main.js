2c2db7eaa3f99cb0647d5a4a18bd7b120ca52bf6
vagrant <vagrant@vagrant.vm>
2016-10-12 00:42:23 +0000

    Add spinner image when loading and error handling for bad URLs

--- static/main.js
+++ static/main.js
@@ -4,6 +4,9 @@
 	angular.module('WordcountApp', [])
 		.controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
 			function($scope, $log, $http, $timeout){
+				$scope.submitButtonText = 'Submit'
+				$scope.loading = false
+				$scope.urlerror = false
 				function getWordCount(jobID){
 					var timeout = ""
 					var poller = function(){
@@ -12,12 +15,19 @@
 								if(status === 202){
 									$log.log(data, status)
 								} else if(status === 200){
+									$scope.loading = false
+									$scope.submitButtonText = 'Submit'
 									$scope.wordcounts = data
 									$timeout.cancel(timeout)
 									return false
 								}
 								//call poller till timeout is cancelled
 								timeout = $timeout(poller, 2000)
+							})
+							.error(function(error){
+								$scope.urlerror = true
+								$scope.submitButtonText = 'Submit'
+								$scope.loading = false
 							})
 					}
 					poller()
@@ -31,6 +41,8 @@
 						.success(function(results){
 							$log.log(results)
 							getWordCount(results)
+							$scope.loading = true
+							$scope.submitButtonText = 'Loading...'
 						}).error(function(error){
 							$log.log(error)
 						});
